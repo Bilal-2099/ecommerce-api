d654c7dcd6cf9d793280c35cbac710d82d7cd32d
Bilal-2099 <bil321al@gmail.com>
2026-05-08 01:28:25 +0500

    Probably last update

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+
 admin.site.register(CustomUser)
 admin.site.register(Product)
 admin.site.register(Category)
